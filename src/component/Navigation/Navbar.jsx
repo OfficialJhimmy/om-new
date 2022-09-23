f6ee74286e8f2f4facccaafc4e2fe0098de03dd0
jhimmyofficial <olufemijhimmy@gmail.com>
2022-09-23 13:11:13 +0100

    added home

--- src/component/Navigation/Navbar.jsx
+++ src/component/Navigation/Navbar.jsx
@@ -58,6 +58,16 @@ function Navbar() {
           )}
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+            <Link
+              to="/"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Home
+            </Link>
+          </li>
+          
           <li className="nav-item">
             <Link
               to="/insights"
