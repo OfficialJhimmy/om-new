fe6886a99645d4ff53df6f4815f617f05ed78899
jhimmyofficial <olufemijhimmy@gmail.com>
2022-09-23 13:20:33 +0100

    added home

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Banner.css";
 import BannerVideo from "../../assets/videos/banner-video.mp4";
-import BannerVideoWeb from '../../assets/videos/home-banner-webm.webm'
+import BannerVideoWeb from "../../assets/videos/home-banner-webm.webm";
 import Button from "../Button/Button";
-import Idea from '../../assets/icons/light-bulb.png'
+import Idea from "../../assets/icons/light-bulb.png";
 
 const Banner = () => {
   return (
@@ -19,13 +19,14 @@ const Banner = () => {
         <div className="banner__content">
           <h1>
             Leading the Way in <br />
-            Financial Services.
+            Professional Services.
           </h1>
           <p>
-            We are a team of professionals with years of valuable experiences who are capable of solving complex business issues and provide solutions to financial challenges .
+            We are a team of professionals with years of valuable experiences
+            who are capable of solving complex business issues and to provide solutions to accounting challenges.
           </p>
-          
-          <Button title='Our Insights' icon={Idea} url='/insights' />
+
+          <Button title="Our Insights" icon={Idea} url="/insights" />
         </div>
       </div>
     </>
